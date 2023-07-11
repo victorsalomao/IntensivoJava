c6b4bceeb7593d0dc44ad19db083e0c0979ecae1
Victor Salomão <victor.salomaor@gmail.com>
2023-07-10 21:30:16 -0300

    Criação do Projeto e pastas e pontos relacionados

---
+++ src/main/java/com/devsuperior/dslist/JavaIntensivoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIntensivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIntensivoApplication.class, args);
+	}
+
+}
